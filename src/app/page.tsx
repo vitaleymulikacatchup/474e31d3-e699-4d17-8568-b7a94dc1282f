e54081873fe6c48806e106d385ec467191effa62
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-24 14:15:35 +0300

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import SocialsAbout from '@/components/sections/layouts/about/SocialsAbout';
 import HowToBuy2D from '@/components/sections/layouts/howtobuy/2DHTB';
 import BigNumberTokenomics from '@/components/sections/layouts/tokenomics/BigNumberTokenomics';
 import FooterBase from '@/components/footer/FooterBase';
+import { LucideIcon } from 'lucide-react';
 
 const navItems = [
   { name: 'Hero', id: 'hero' },
